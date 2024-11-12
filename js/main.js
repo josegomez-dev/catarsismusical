c48a0ba438f4b93779c679e629fb5535bd858400
Jose Alejandro Gomez Castro <josealejandrogomezcastro@Joses-MacBook-Pro.local>
2024-11-11 21:04:11 -0600

    add some lazy loader

--- js/main.js
+++ js/main.js
@@ -226,3 +226,29 @@ $(document).ready(function () {
 		$('html, body').animate({ scrollTop: 0 }, '300');
 	});
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+	const lazyImages = document.querySelectorAll("img.lazy");
+
+	if ("IntersectionObserver" in window) {
+			let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+					entries.forEach(function (entry) {
+							if (entry.isIntersecting) {
+									let lazyImage = entry.target;
+									lazyImage.src = lazyImage.dataset.src;
+									lazyImage.classList.remove("lazy");
+									lazyImageObserver.unobserve(lazyImage);
+							}
+					});
+			});
+
+			lazyImages.forEach(function (lazyImage) {
+					lazyImageObserver.observe(lazyImage);
+			});
+	} else {
+			// Fallback for older browsers
+			lazyImages.forEach(function (lazyImage) {
+					lazyImage.src = lazyImage.dataset.src;
+			});
+	}
+});
